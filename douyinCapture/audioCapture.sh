1005e2cd19d297918858a24ba6964a2228e88ebf
Gu <gu@192.168.124.12>
2019-12-16 22:46:37 +0800

    douyin capture added

---
+++ douyinCapture/audioCapture.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cd ..
+git pull
+cd -
+
+i=0
+while read line
+do
+  if [[ $line == https* || $line == http* || $line == www* ]]
+  then
+    URL_LST[$i]=$line
+    i=$((i+1))
+  fi
+done < url.txt
+
+dir=$(date +%Y_%m%d_%H%M_%S)
+
+mkdir $dir
+
+cp url.txt $dir/url.txt
+
+url_lst_len=${#URL_LST[*]}
+
+echo -e "file list: \n\r" > $dir/file_list.txt
+
+url_pnt=0
+while [ $url_pnt -lt $url_lst_len ]
+do
+
+  URL=${URL_LST[ $((url_pnt + 0)) ]}
+
+  echo -n " audio$url_pnt :" >> $dir/file_list.txt
+
+  annie -i -o $dir -O audio$url_pnt $URL | grep "Title" | sed 's/Title://g' >> $dir/file_list.txt
+
+  echo -e "\n\r" >> $dir/file_list.txt
+
+  url_pnt=$((url_pnt + 1))
+
+done
+
+url_pnt=0
+while [ $url_pnt -lt $url_lst_len ]
+do
+
+  echo -e "\n\r正在下载第$((url_pnt+1))个视频, 总共$url_lst_len个视频"
+
+  URL=${URL_LST[ $((url_pnt + 0)) ]}
+
+  annie -o $dir -O audio$url_pnt $URL
+  
+  ffmpeg -i $dir/audio$url_pnt.mp4 $dir/audio$url_pnt.mp3
+
+  url_pnt=$((url_pnt + 1))
+
+done
+
